67c891c44b48609303942102c6efbf1964e56b96
Brent Wilske <wilskeworkflow@gmail.com>
2022-01-24 09:40:05 -0800

    added modal to input books

--- app.js
+++ app.js
@@ -14,7 +14,34 @@ Book.prototype.logInfo= function () {
 
 // let theHobbit = new Book("The Hobbit","JRR Tolkein", 815, true)
 
-function addBookToLibrary() {
-    const newBook = book
+
+
+function addBookToLibrary(title, author, pages, hasRead) {
+    const newBook = Book(title, author, pages, hasRead);
+    myLibrary.push(newBook);
 }
+
+function openModal(){
+    modal.style.display = "block";
+}
+
+function closeModal(){
+    modal.style.display = "none";
+}
+
+function clickOutside(e){
+    if(e.target == modal){
+        modal.style.display = "none";
+    }
+}
+
+const modal = document.getElementById("bookEntryModal");
+const modalBtn = document.getElementById("modalBtn");
+const closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+modalBtn.addEventListener("click", openModal);
+
+closeBtn.addEventListener("click", closeModal);
+
+window.addEventListener("click", clickOutside)
 
