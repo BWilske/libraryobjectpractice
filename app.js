ecbbc470f5425fe5d325e530f3a2f476542b29d5
Brent Wilske <wilskeworkflow@gmail.com>
2022-01-25 20:02:48 -0800

    pushing to work on laptop

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let myLibrary = []
+let myLibrary = [];
 
 //book generator
 function Book(title, author, pages, hasRead ) {
@@ -9,40 +9,40 @@ function Book(title, author, pages, hasRead ) {
 }
 Book.prototype.returnInfo= function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead}`
-}
+};
 
-myLibrary[0] = new Book("The Hobbit","JRR Tolkein", 815, false)
-myLibrary[1] = new Book("The Hobbit","JRR Tolkein", 815, false)
-myLibrary[2] = new Book("The Hobbit","JRR Tolkein", 815, false)
-myLibrary[3] = new Book("The Hobbit","JRR Tolkein", 815, false)
-myLibrary[4] = new Book("The Hobbit","JRR Tolkein", 815, false)
-myLibrary[5] = new Book("The Hobbit","JRR Tolkein", 815, false)
-myLibrary[6] = new Book("The Hobbit","JRR Tolkein", 815, true)
-myLibrary[7] = new Book("The Hobbit","JRR Tolkein", 815, true)
-myLibrary[8] = new Book("The Hobbit","JRR Tolkein", 815, true)
-myLibrary[9] = new Book("The Hobbit","JRR Tolkein", 815, true)
+myLibrary[0] = new Book("The Hobbit","JRR Tolkein", 815, false);
+myLibrary[1] = new Book("The Hobbit","JRR Tolkein", 81532455, false);
+myLibrary[2] = new Book("The Hobbit","JRR Tolfewqfwekein", 815, false);
+myLibrary[3] = new Book("The Hofweqfwqefebbit","JRR Tolkein", 815, false);
+myLibrary[4] = new Book("The Hobbit","JRR Tolkein", 815, false);
+myLibrary[5] = new Book("The Hobbit","JRR Tolkein", 815, false);
+myLibrary[6] = new Book("The Hobbit","JRR Tolkein", 815, true);
+myLibrary[7] = new Book("The Hobbit","JRR Tolkein", 815, true);
+myLibrary[8] = new Book("The Hobbit","JRR Tolkein", 815, true);
+myLibrary[9] = new Book("The Hobbit","JRR Tolkein", 815, true);
 
 //takes in info from form to create new book
-function addBookToLibrary(title, author, pages, hasRead) {
+function addBookToLibrary([title, author, pages, hasRead]) {
     const newBook = Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
 }
 //loops through books and displays
 function displayBooks(library) {
-    let libraryEl = document.getElementById("library-container")
+    let libraryEl = document.getElementById("library-container");
     for(let i=0; i < library.length; ++i) {
         
-        let newBook = document.createElement("div")
-        newBook.setAttribute("class", "book")
+        let newBook = document.createElement("div");
+        newBook.setAttribute("class", "book");
 
-        let hasReadasText = boolTotext(library[i].hasRead)
+        let hasReadasText = boolTotext(library[i].hasRead);
 
         newBook.innerHTML = `
         <p>Title: ${library[i].title}</p>
         <p>Author: ${library[i].author}</p>
         <p>Pages: ${library[i].pages}</p>
         <p>${hasReadasText}</p>`
-        libraryEl.append(newBook)
+        libraryEl.append(newBook);
         
 
         
@@ -50,23 +50,30 @@ function displayBooks(library) {
 }
 //converts a bool in array to the text for display on page
 function boolTotext(hasRead) {
-    console.log(hasRead)
     if (hasRead===true)  return "Read";
-    else return "Not Read"
+    else return "Not Read";
 }
 
 // modal
 const Modal = {
-
+    // display
     container:document.getElementById("bookEntryModal"),
     button:document.getElementById("modalBtn"),
     closeBtn:document.getElementsByClassName("closeBtn")[0],
+    // grabbing data
+    dataGrabber:document.getElementsByClassName("bookinfo"),
+    enterBtn:document.getElementById("enterBtn"),
+
+
 
     addEvent: function() {
+        //display
         Modal.button.addEventListener("click", Modal.open);
         Modal.closeBtn.addEventListener("click", Modal.close);
         window.addEventListener("click", Modal.clickOutside);
-        // console.log("added events")
+        // grabbing data
+        Modal.enterBtn.addEventListener("click", addBookToLibrary(Modal.dataGrabber) )
+        console.log(Modal.dataGrabber)
     },
 
     open: () => {
@@ -93,4 +100,4 @@ const Modal = {
 }
 
 
-Modal.addEvent()
+Modal.addEvent();
