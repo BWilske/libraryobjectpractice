fcedf71ed583f37b62e355140c0d882bd91d0ba7
Brent Wilske <wilskeworkflow@gmail.com>
2022-07-05 10:11:39 -0700

    refactored with classes

--- app.js
+++ app.js
@@ -1,103 +1,86 @@
 let myLibrary = [];
 
 //book generator
-function Book(title, author, pages, hasRead ) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.hasRead = hasRead
+class Book {
+  constructor(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+  }
+
+  returnInfo = () => {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead}`;
+  };
 }
-Book.prototype.returnInfo= function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead}`
-};
 
-myLibrary[0] = new Book("The Hobbit","JRR Tolkein", 815, false);
-myLibrary[1] = new Book("The Hobbit","JRR Tolkein", 81532455, false);
-myLibrary[2] = new Book("The Hobbit","JRR Tolfewqfwekein", 815, false);
-myLibrary[3] = new Book("The Hofweqfwqefebbit","JRR Tolkein", 815, false);
-myLibrary[4] = new Book("The Hobbit","JRR Tolkein", 815, false);
-myLibrary[5] = new Book("The Hobbit","JRR Tolkein", 815, false);
-myLibrary[6] = new Book("The Hobbit","JRR Tolkein", 815, true);
-myLibrary[7] = new Book("The Hobbit","JRR Tolkein", 815, true);
-myLibrary[8] = new Book("The Hobbit","JRR Tolkein", 815, true);
-myLibrary[9] = new Book("The Hobbit","JRR Tolkein", 815, true);
+for (let i = 0; i <= 10; i++) {
+  myLibrary[i] = new Book("The Hobbit", "JRR Tolkein", 815, false);
+}
 
-//takes in info from form to create new book
-function addBookToLibrary([title, author, pages, hasRead]) {
-    const newBook = Book(title, author, pages, hasRead);
-    myLibrary.push(newBook);
+//takes in info from form to create new book ///order for data [title,author,pages,hasRead(bool)]
+function addBookToLibrary(...data) {
+  const newBook = new Book(...data);
+  myLibrary.push(newBook);
 }
 //loops through books and displays
 function displayBooks(library) {
-    let libraryEl = document.getElementById("library-container");
-    for(let i=0; i < library.length; ++i) {
-        
-        let newBook = document.createElement("div");
-        newBook.setAttribute("class", "book");
+  const libraryEl = document.getElementById("library-container");
+  for (let i = 0; i < library.length; ++i) {
+    const newBook = document.createElement("div");
+    newBook.setAttribute("class", "book");
 
-        let hasReadasText = boolTotext(library[i].hasRead);
+    let hasReadasText = boolTotext(library[i].hasRead);
 
-        newBook.innerHTML = `
+    newBook.innerHTML = `
         <p>Title: ${library[i].title}</p>
         <p>Author: ${library[i].author}</p>
         <p>Pages: ${library[i].pages}</p>
-        <p>${hasReadasText}</p>`
-        libraryEl.append(newBook);
-        
-
-        
-    }
+        <p>${hasReadasText}</p>`;
+    libraryEl.append(newBook);
+  }
 }
 //converts a bool in array to the text for display on page
 function boolTotext(hasRead) {
-    if (hasRead===true)  return "Read";
-    else return "Not Read";
+  if (hasRead === true) return "Read";
+  else return "Not Read";
 }
 
 // modal
 const Modal = {
-    // display
-    container:document.getElementById("bookEntryModal"),
-    button:document.getElementById("modalBtn"),
-    closeBtn:document.getElementsByClassName("closeBtn")[0],
+  // display
+  container: document.getElementById("bookEntryModal"),
+  button: document.getElementById("modalBtn"),
+  closeBtn: document.getElementsByClassName("closeBtn")[0],
+  // grabbing data
+  dataGrabber: document.getElementsByClassName("bookinfo"),
+  enterBtn: document.getElementById("enterBtn"),
+
+  addEvent: function () {
+    //display
+    Modal.button.addEventListener("click", Modal.open);
+    Modal.closeBtn.addEventListener("click", Modal.close);
+    window.addEventListener("click", Modal.clickOutside);
     // grabbing data
-    dataGrabber:document.getElementsByClassName("bookinfo"),
-    enterBtn:document.getElementById("enterBtn"),
+    Modal.enterBtn.addEventListener(
+      "click",
+      addBookToLibrary(Modal.dataGrabber)
+    );
+  },
 
+  open: () => {
+    Modal.container.style.display = "block";
+  },
 
+  close: () => {
+    Modal.container.style.display = "none";
+  },
 
-    addEvent: function() {
-        //display
-        Modal.button.addEventListener("click", Modal.open);
-        Modal.closeBtn.addEventListener("click", Modal.close);
-        window.addEventListener("click", Modal.clickOutside);
-        // grabbing data
-        Modal.enterBtn.addEventListener("click", addBookToLibrary(Modal.dataGrabber) )
-        console.log(Modal.dataGrabber)
-    },
-
-    open: () => {
-        console.log("test")
-       Modal.container.style.display = "block";
-        
-        
-    },
-    
-    close: () => {
-        Modal.container.style.display = "none";
-    },
-
-    clickOutside: (e) =>{
-        if(e.target == Modal.container){
-            Modal.container.style.display = "none";
-        }
+  clickOutside: (e) => {
+    if (e.target == Modal.container) {
+      Modal.container.style.display = "none";
     }
-
-    
-
-    
-
-}
-
+  },
+};
 
 Modal.addEvent();
